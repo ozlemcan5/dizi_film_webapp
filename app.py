8fc52d29200da393e2f8af786e2c8ee8cd10b568
ozlemcan5 <115418399+ozlemcan5@users.noreply.github.com>
2026-09-10 16:23:58 +0300

    Update app.py

--- app.py
+++ app.py
@@ -9,8 +9,7 @@ import sys
 logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'SECRET_KEY'
-secret_key = os.environ.get('SECRET_KEY')
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
 
 database_url = os.environ.get('DATABASE_URL')
 if database_url and database_url.startswith('postgres://'):
