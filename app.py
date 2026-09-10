32a2991ddf6eb9caec8b330c8b99b519d87095e7
ozlemcan5 <115418399+ozlemcan5@users.noreply.github.com>
2026-09-10 16:29:09 +0300

    Update app.py

--- app.py
+++ app.py
@@ -12,8 +12,6 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
 
 database_url = os.environ.get('DATABASE_URL')
-if database_url and database_url.startswith('postgres://'):
-  database_url = database_url.replace('postgres://', 'postgresql://', 1)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = (
     database_url or 'sqlite:///database.db'
