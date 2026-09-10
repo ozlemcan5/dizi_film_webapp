01ff6c18661f05e72e97d294522fa76eb80f4ec1
ozlemcan5 <115418399+ozlemcan5@users.noreply.github.com>
2026-09-10 15:01:16 +0300

    Update app.py

--- app.py
+++ app.py
@@ -8,10 +8,16 @@ import sys
 
 logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
 
-
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'gizli-anahtar-kelime'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+
+database_url = os.environ.get('DATABASE_URL')
+if database_url and database_url.startswith('postgres://'):
+  database_url = database_url.replace('postgres://', 'postgresql://', 1)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = (
+    database_url or 'sqlite:///database.db'
+)
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
